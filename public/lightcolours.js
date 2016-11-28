4286a3dc6404ecaa4324fd74f470ef6a0f74cfce
Seb Lee-Delisle <sebleedelisle@gmail.com>
2016-11-28 16:43:45 +0000

    adding public folder

---
+++ public/lightcolours.js
@@ -0,0 +1,37 @@
+
+var colours = {
+	white : {h:20, s:80, l:80},
+	blue : {h:190, s:100, l:50},
+	red : {h:340, s:80, l:50},
+	green : {h:170, s:90, l:50},
+	yellow : {h:30, s:100, l:50},
+}
+var lightColours = [ 
+	'white', 	// A
+	'blue', 	// B
+	'red', 		// C
+	'green', 	// D
+	'blue', 	// E
+	'yellow', 	// F
+	'red',		// G
+	'green',	// H
+	'green',	// I
+	'red',		// J
+	'blue',		// K
+	'green', 	// L
+	'yellow',	// M
+	'red',		// N
+	'red', 		// O
+	'green', 	// P
+	'red', 		// Q
+	'green',	// R
+	'white',	// S
+	'yellow', 	// T
+	'blue', 	// U
+	'red',		// V
+	'blue',		// W
+	'yellow',	// X
+	'red',		// Y
+	'red', 		// Z
+	
+];
